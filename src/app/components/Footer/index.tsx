a0c7d111b458be26e119d9b35cc6c427dfbab55c
Michael Kelly <20041540@mail.wit.ie>
2024-07-19 10:12:29 +0100

    Create footer in storybook

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -4,15 +4,19 @@ import '../../styles/Footer.scss';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
+      <hr />
+      <div className="logo">FRONTEND SCHOOL</div>
       <div className="container">
-        <p>&copy; 2023 Frontend School</p>
-        <ul>
-          <li><a href="#privacy-policy">Privacy Policy</a></li>
-                  <li><a href="#terms-of-use">Terms of Use</a></li>
-                  <li><a href="#contact-us">Contact Us</a></li>
-              </ul>
-          </div>
-      </footer>
+        <nav>
+          <ul>
+            <li><a href="#privacy-policy">Privacy Policy</a></li>
+            <li><a href="#terms-of-use">Terms of Use</a></li>
+            <li><a href="#contact-us">Contact Us</a></li>
+          </ul>
+        </nav>
+        <div className="copyright">&copy; Frontend 2023</div>
+      </div>
+    </footer>
   );
 }
 
